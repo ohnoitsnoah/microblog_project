67149f4398bb526eb52ca6320f10cfc050a842dd
Noah <nerdlycartoonist@gmail.com>
2025-05-04 23:15:59 -0500

    syntax fixes

--- terracotta/main.go
+++ terracotta/main.go
@@ -1,46 +1,51 @@
 package main
 
-import(
-	"html/template"
-	"log"
-	"net/http"
-	"sync"
+import (
+    "html/template"
+    "log"
+    "net/http"
+    "sync"
 )
 
 type Post struct {
-	Username string
-	Content string
+    Username string
+    Content  string
 }
 
-var(
-	posts []Post
-	mu    sync.Mutex
-	tmpl  *template.Template
+var (
+    posts []Post
+    mu    sync.Mutex
+    tmpl  *template.Template
 )
 
 func main() {
-	tmpl = template.Must(template.Parse.Glob("templates/*.html"))
+    tmpl = template.Must(template.ParseGlob("templates/*.html"))
 
-	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/post" postHandler)
+    http.HandleFunc("/", indexHandler)
+    http.HandleFunc("/post", postHandler)
 
-	log.Println("Starting server on :8080...")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+    log.Println("Starting server on :8081...")
+    log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+    mu.Lock()
+    defer mu.Unlock()
+    tmpl.ExecuteTemplate(w, "layout.html", posts)
 }
 
 func postHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Redirect(w, r, "/", http.StatusSeeOther)
-		return
-	}
+    if r.Method != http.MethodPost {
+        http.Redirect(w, r, "/", http.StatusSeeOther)
+        return
+    }
 
-	username := r.FormValue("username")
-	content := r.FormValue("content")
+    username := r.FormValue("username")
+    content := r.FormValue("content")
 
-	mu.Lock()
-	posts = append([]Post{{Username: username, Content: content}}, posts...)
-	// Allows for new posts to show up on top
-	mu.Unlock()
+    mu.Lock()
+    posts = append([]Post{{Username: username, Content: content}}, posts...) // newest first
+    mu.Unlock()
 
-	http.Redirect(w, r, "/", http.StatusSeeOther)
+    http.Redirect(w, r, "/", http.StatusSeeOther)
 }
